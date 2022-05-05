7cde329a385184c307a943bb064b53c8ac8108ab
Oliver <94859820+oliverGarniu@users.noreply.github.com>
2022-05-05 10:43:54 +0200

    Delete S5_SpringAvançat/S5.02 directory

--- S5_SpringAvançat/S5.02/S502.n3/S502.MySQL_MongoDB/src/main/java/com/jocdedaus/demo/repository/JugadorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jocdedaus.demo.repository;
-
-import com.jocdedaus.demo.entity.Jugador;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JugadorRepository extends JpaRepository<Jugador, Integer> {
-}
--- S5_SpringAvançat/S5.02/S502.n3/S502.MySQL_MongoDB/src/main/java/com/jocdedaus/demo/repository/PartidaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jocdedaus.demo.repository;
-
-import com.jocdedaus.demo.document.Partida;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PartidaRepository extends MongoRepository<Partida, Integer> {
-}
